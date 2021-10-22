9d8ff52b57767829d74405f31fec7616aee5e30b
bryancorpse <al221610173@gmail.com>
2021-10-22 17:32:41 -0500

    news category

--- resources/views/front/components/newsCategory.blade.php
+++ resources/views/front/components/newsCategory.blade.php
@@ -1,8 +1,8 @@
 <div class="uk-card uk-margin-small-bottom">
     @if($category->tipo_id == 1 || $category->tipo_id == 4)
         @foreach ($category->images as $key =>  $image)
-     
-   
+            @if ($key == 0)
+            <div class="b-card-category ">
                 <a href="{{ route('showArticle', ['category' => $category->category->slug, 'slug' => $category->slug]) }}" title="{{ $category->title }}">
 
                     <img class="b-image" src="{{ asset('storage' . '/' . $category->user->name . '/'. $image->name ) }}" alt="{{ $category->title }}" width="400">
@@ -10,12 +10,14 @@
                 <div class="uk-card-footer uk-card-default b-click-footer">
                     {{ Str::limit($category->title, 250) }}
                 </div>
+            </div>
+            <br>
                     {{-- <div class="b-category-center uk-h6">
                         {{ Str::limit($category->title, 80) }}
                     </div> --}}
                    
                     {{-- @include('front.partials.smallSharelinks', ['article' => $category]) --}}
- 
+            @endif
        
         @endforeach  
 
