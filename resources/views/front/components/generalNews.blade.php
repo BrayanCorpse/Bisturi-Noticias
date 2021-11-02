7c1d1f1f4b61a7e3b325b0d59e9628d982d535fb
bryancorpse <al221610173@gmail.com>
2021-11-02 13:26:34 -0600

    badge

--- resources/views/front/components/generalNews.blade.php
+++ resources/views/front/components/generalNews.blade.php
@@ -2,7 +2,9 @@
     @foreach ($general->images as $key =>  $image)
         @if ($key == 0)
             <div class="uk-card show-card-body uk-position-relative">
-                <span class="b-span-generals b-span-text">{{ $general->category->name}}</span>
+                <a class="uk-link-reset" href="{{url($general->category->slug)}}">
+                    <span class="b-span-generals b-span-text">{{ $general->category->name}}</span>
+                </a>
                 <a href="{{ route('showArticle', ['category' => $general->category->slug, 'slug' => $general->slug]) }}" title="{{ $general->title }}">
 
                     <img loading="lazy" class="b-img-general" src="{{ asset('storage' . '/' . $general->user->name . '/'. $image->name ) }}" alt="{{ $general->title }}" width="225" height="225">
